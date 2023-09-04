95859b5369090725ae5b8118db7c88e8e9af1394
VeryNessa <122435256+VeryNessa@users.noreply.github.com>
2023-09-04 20:45:15 +0200

    Create 1_1_4-DansLeFourré.java

---
+++ 1_1_4-DansLeFourré.java
@@ -0,0 +1,14 @@
+import static algorea.Robot.*;
+ 
+class Main {
+   public static void main(String[] args) {
+      haut();
+      haut();
+      haut();
+      droite();
+      droite();
+      bas();
+      bas();
+      droite();
+   }
+}
