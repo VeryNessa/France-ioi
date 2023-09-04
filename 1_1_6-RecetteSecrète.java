e5980e2edf17ff7993b5e3cfec9b60eecb0f7079
VeryNessa <122435256+VeryNessa@users.noreply.github.com>
2023-09-04 20:46:33 +0200

    Create 1_1_6-RecetteSecrète.java

---
+++ 1_1_6-RecetteSecrète.java
@@ -0,0 +1,14 @@
+import static algorea.Robot.*;
+
+class Main {
+   public static void main(String[] args) {
+      remplir(3);
+      transferer(3, 5);
+      remplir(3);
+      transferer(3,5);
+      vider(5);
+      transferer(3,5);
+      remplir(3);
+      transferer(3,5);
+   }
+}
