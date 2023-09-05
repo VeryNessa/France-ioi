89f8eebf900d2ddbd1076ef796759305f090aacd
VeryNessa <122435256+VeryNessa@users.noreply.github.com>
2023-09-05 19:14:53 +0200

    Create 04-EncoreDesPunitions.java

---
+++ Niveau_1/4-LectureDeLEntree/04-EncoreDesPunitions.java
@@ -0,0 +1,17 @@
+import algorea.Scanner;
+
+class Main{
+
+   static Scanner entrée = new Scanner (System.in);
+   
+   public static void main(String[] args){
+   
+      int nbLignes = entrée.nextInt();
+      
+      for (int i = 0; i < nbLignes; i++){
+      
+      System.out.println("Je dois suivre en cours");
+      
+      }
+   }
+}
