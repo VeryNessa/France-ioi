638b62c8585f91557c50ca07c15d2b4330571030
VeryNessa <122435256+VeryNessa@users.noreply.github.com>
2023-09-05 19:06:36 +0200

    Create 5-CourDeRecreation.java

---
+++ Niveau_1/3-CalculsEtDecouverteDeVariables/5-CourDeRecreation.java
@@ -0,0 +1,7 @@
+class Main{
+   public static void main(String[] args){
+      int longueur=5*17+2*7+1*5+2*2;
+      System.out.print(longueur*longueur + " ");
+      System.out.print(4*longueur);
+    }
+ }
