f223b48abdff180e68ec8dfbf64e2f8ea2457375
VeryNessa <122435256+VeryNessa@users.noreply.github.com>
2023-09-05 19:08:05 +0200

    Create 9-InvasionDeBatraciens.java

---
+++ Niveau_1/3-CalculsEtDecouverteDeVariables/9-InvasionDeBatraciens.java
@@ -0,0 +1,10 @@
+class Main{
+   public static void main(String[] args){
+      int nbcrapauds= 1337;
+      
+      for(int i = 1; i <= 12; i++){
+         nbcrapauds*=2;
+      }
+      System.out.println(nbcrapauds);
+   }
+}
