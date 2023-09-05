20653e0ae6a15cbf5f5786b5c29e78d09aa79bbe
VeryNessa <122435256+VeryNessa@users.noreply.github.com>
2023-09-05 19:08:26 +0200

    Create 10-Kermesse.java

---
+++ Niveau_1/3-CalculsEtDecouverteDeVariables/10-Kermesse.java
@@ -0,0 +1,9 @@
+class Main{
+   public static void main(String[] args){
+      int bonbons=0;
+      for (int i =1; i<51; i++){
+         bonbons +=i;
+         System.out.println(bonbons);
+      }
+   }
+}
