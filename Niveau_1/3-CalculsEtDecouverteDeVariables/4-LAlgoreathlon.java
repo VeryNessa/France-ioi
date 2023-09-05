e56381f60731536ec75f79c88bcdc8e169d0b29d
VeryNessa <122435256+VeryNessa@users.noreply.github.com>
2023-09-05 19:06:12 +0200

    Create 4-LAlgoreathlon.java

---
+++ Niveau_1/3-CalculsEtDecouverteDeVariables/4-LAlgoreathlon.java
@@ -0,0 +1,8 @@
+class Main{
+   public static void main(String[] args){
+      int distance = 2 + 34 + 6;
+      System.out.print(distance + " ");
+      System.out.print(2*distance + " ");
+      System.out.print (3*distance);
+   }
+}
