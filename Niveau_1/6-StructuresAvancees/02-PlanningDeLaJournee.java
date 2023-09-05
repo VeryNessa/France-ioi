fcddf2a13e8ecaf78ff35b039a79e8daed4a4580
VeryNessa <122435256+VeryNessa@users.noreply.github.com>
2023-09-05 19:23:49 +0200

    Create 02-PlanningDeLaJournee.java

---
+++ Niveau_1/6-StructuresAvancees/02-PlanningDeLaJournee.java
@@ -0,0 +1,31 @@
+/*Error*/
+
+import algorea.Scanner;
+
+class Main {
+   static Scanner entrée = new Scanner(System.in);
+   public static void main(String[] args) {
+      
+      int position = entrée.nextInt();  
+      
+      int nbVillage = entrée.nextInt();
+      
+      int cmpt=0;
+      
+      for (int i     = 1; i <= nbVillage; i++){
+        
+         int posVillage = entrée.nextInt();
+         
+         if (posVillage <= (position+50)){
+         
+            if (posVillage>=(position-50)){
+        
+         cmpt=cmpt+1;
+        
+         }
+         }
+      }
+      
+      System.out.print(cmpt);
+   }
+}
